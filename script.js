5467179cb2dacf73d3f9b9d6b178f45ba342b300
AnaHeloisaLp <00001081279291sp@al.educacao.sp.gov.br>
2024-08-27 08:13:29 -0300

    ggf

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click',function(){
-        const atual = document.querySelector('ativo');
-        const proximoPasso = 'passo' + this.getAttribute('data-proximo');
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
         
         atual.classList.remover('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
