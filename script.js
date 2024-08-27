6838f7a59825815d9451634f3c2d77e4e6b688cf
AnaHeloisaLp <00001081279291sp@al.educacao.sp.gov.br>
2024-08-27 07:32:50 -0300

    Update script

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const botoesProximo = document.querySelectorAll('.btn-proximo');
-    const passos = document.querySelectorAll('.passo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-    botoesProximo.forEach(botao => {
-        botao.addEventListener('click', () => {
-            const proximoPasso = botao.getAttribute('data-proximo');
-            passos.forEach(passo => {
-                passo.classList.remove('ativo');
-            });
-            document.getElementById(`passo-${proximoPasso}`).classList.add('ativo');
-        });
-    });
-});
+avanca.forEach(button => {
+    button.addEventListener('click',function(){
+        const atual = document.querySelector('ativo');
+        const proximoPasso = 'passo' + this.getAttribute('data-proximo');
+        
+        atual.classList.remover('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+           )}
+)}
+        
